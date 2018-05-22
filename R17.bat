406360201d3d8cec8fb353562e879d91cb26fe39
Y-h. Taguchi <tag@granular.com>
2018-05-22 21:05:37 +0900

    Add files via upload

---
+++ R17.bat
@@ -0,0 +1,18 @@
+x <- read.csv("GSE59525_RPKM_Pcan.txt.gz",sep="\t")
+load("x1_Pcan76W")
+S <- split(1:dim(x1)[1],x1[,1])
+SS <- list(rep(NA,length(S)))
+for (i in c(1:length(S)))
+{
+cat(i, " ")
+SS[[i]] <- split(S[[i]],x1[S[[i]],3])
+}
+methyl <- NULL
+for (i in c(1:dim(x)[1]))
+{
+cat(i," ")
+if (x[i,5]=="+") {is<-1} else {is<-2}
+index<-  SS[[x[i,2]]][[is]][x1[SS[[x[i,2]]][[is]],2]>=x[i,3] & x1[SS[[x[i,2]]][[is]],2]<=x[i,4]]
+methyl <- rbind(methyl,data.frame(x[i,1],t(colSums(x1[index,4:5]))))
+}
+save(file="methyl_Pcan76W",methyl)
---
+++ R27.bat
@@ -0,0 +1,23 @@
+x <- read.csv("GSE59525_RPKM_Dqua.txt.gz",sep="\t")
+load("x1_Dqua3DW")
+S <- split(1:dim(x1)[1],x1[,1])
+SS <- list(rep(NA,length(S)))
+for (i in c(1:length(S)))
+{
+cat(i, " ")
+SS[[i]] <- split(S[[i]],x1[S[[i]],3])
+}
+gtf <- read.csv("DQUA.v02.gff3",sep="\t",comment.char="#",header=F) #Dqua
+gtf1 <- gtf[grep("gene_name",gtf[,9]),] #Dqua
+
+methyl <- NULL
+for (i in c(1:dim(x)[1]))
+{
+cat(i," ")
+if (x[i,5]=="-") {is<-1} else {is<-2} #Dqua
+jj <- grep(x[i,1],gtf1[,9])[1] #Dqua
+ii <- match(gtf1[jj,1],names(S)) #Dqua
+index<-  SS[[ii]][[is]][x1[SS[[ii]][[is]],2]>=gtf1[jj,4] & x1[SS[[ii]][[is]],2]<=gtf1[jj,5]] #Dqua
+methyl <- rbind(methyl,data.frame(x[i,1],t(colSums(x1[index,4:5]))))
+}
+save(file="methyl_Dqua3DW",methyl)
